0306626098e157065f55b059df7c31bf477ee285
Chris Bennett <110393183+oev-chrisbennett@users.noreply.github.com>
2024-04-22 00:58:47 +0100

    chore: styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,11 @@ module.exports = {
                 'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
             },
         },
+        fontSize: {
+            xs: '0.75rem',
+            sm: '0.875rem',
+            base: '1rem',
+        },
     },
     plugins: [],
 }
